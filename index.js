8ccedea560b67bcd3bb8915ce58a13e2f753f272
Drew Benson <Drewbenson16@gmail.com>
2024-01-02 11:44:20 -0700

    wow

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      return response.json();
+    })
+    .then(json => {
+      renderBooks(json);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  if (!main) {
+    console.log('Could not find the main element');
+    return;
+  }
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
@@ -12,6 +27,7 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   fetchBooks();
 });
+
